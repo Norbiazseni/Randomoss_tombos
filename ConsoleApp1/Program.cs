eb09e8546965fb0463db45aeb1d2e4066d9445c1
Norbiazseni <131431630+Norbiazseni@users.noreply.github.com>
2024-04-03 20:42:42 +0200

    Tömb feltöltése random számokkal

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,25 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Norbi
+            const int MAXN = 10;
+            int[] szamok = new int[MAXN];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < MAXN; i++)
+            {
+                szamok[i] = rnd.Next(0, 101);
+            }
+            /*
+            foreach (int item in szamok)
+            {
+                Console.Write(item+",");
+            }
+            */
+
+
+            Console.WriteLine("\nNyomjad neki");
+            Console.ReadKey();
         }
     }
 }
